8f4bdaed51beba18d35a268268596c3ef9e75cc0
alicestudies <hs20yp@gmail.com>
2026-03-20 02:02:47 -0400

    Revert "commit"
    This reverts commit 064777f47d0e37160bb560254f688b31abedb093.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,5 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by alicestudies
-# Here is another update by alicestudies
+# File added by <your GitHub username>
